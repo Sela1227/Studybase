e812aa2a1d48a76bf156bab3230c9677f7bd4cb0
Sela1227 <snako.tw@gmail.com>
2026-07-26 10:00:28 +0800

    V2.4.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* StudyBase PWA service worker */
-const CACHE = 'studybase-v2.4.0';
+const CACHE = 'studybase-v2.4.1';
 const CORE = [
   './', './index.html', './css/main.css',
   './chemistry/index.html', './math/index.html', './physics/index.html',
